af372dd8b8b2197bf29010b7bc66f219a54f5575
José Carlos Bezerra Filho <josecarlosbf@gmail.com>
2018-09-25 21:05:04 -0300

    cupom fiscal controller

--- sis/Controller/CupomFiscalController.cs
+++ sis/Controller/CupomFiscalController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,58 @@ namespace Controller
 {
     public class CupomFiscalController
     {
+        public void SalvarCupomFiscal(CupomFiscal cupom)
+        {
+            try
+            {
+                CupomFiscal.Salvar(cupom);
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+        }
+
+        public void ExclirCupomFiscal(int id)
+        {
+            try
+            {
+                CupomFiscal.Remove(id);
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+        }
+        public IList<CupomFiscal> ListarCupomFiscal()
+        {
+            IList<CupomFiscal> ListaCupom;
+            try
+            {
+                ListaCupom = CupomFiscal.ListarCupomFiscals();
+                CupomFiscal.ListarCupomFiscals();
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+            return ListaCupom;
+        }
+
+        public CupomFiscal GetById(int id)
+        {
+
+            CupomFiscal obj = new CupomFiscal();
+            try
+            {
+
+                obj = CupomFiscal.GetById(id);
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+            return obj;
+        }
     }
 }
